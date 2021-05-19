189f12d2d9fd374d769ec435301356ad6cfeeab9
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-05-20 01:36:11 +0530

    Linkedin says that description should be atleast 100 characters long

--- src/constants/SEO.js
+++ src/constants/SEO.js
@@ -2,7 +2,7 @@ export const lang = "en-US";
 export const baseTitle = "Dakshraj Sharma";
 export const author = "Dakshraj Sharma";
 export const metaDescription =
-  "Hi, I'm Dakshraj Sharma, software engineer, code wizard, and serial technical experimenter";
+  "Hi, I'm Dakshraj Sharma, software engineer, code wizard, and serial technical experimenter. Always learning, always improving.";
 export const keywords =
   "Dakshraj Sharma, Dakshraj, software, softeware engineer, India, Developer";
 export const viewport =
