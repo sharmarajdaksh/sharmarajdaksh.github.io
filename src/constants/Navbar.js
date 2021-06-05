38586b1a871b39eee1054c9de20cf3c788f54759
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-06-05 19:27:14 +0530

    Added stackoverflow to sidebar

--- src/constants/Navbar.js
+++ src/constants/Navbar.js
@@ -1,4 +1,9 @@
-import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaMedium,
+  FaStackOverflow,
+} from "react-icons/fa";
 
 export const LINKEDIN_URL = "https://www.linkedin.com/in/sharmarajdaksh/";
 export const GITHUB_URL = "https://github.com/sharmarajdaksh";
@@ -6,6 +11,9 @@ export const MEDIUM_URL = "https://medium.com/@sharmarajdaksh";
 export const EMAIL = "sharmarajdaksh@gmail.com";
 export const GOODREADS_URL =
   "https://www.goodreads.com/user/show/21277948-dakshraj-sharma";
+export const STACKOVERFLOW_URL =
+  "https://stackoverflow.com/users/9134637/dakshraj-sharma";
+
 export const SOCIAL_ICONS_DATA = [
   {
     url: GITHUB_URL,
@@ -18,6 +26,12 @@ export const SOCIAL_ICONS_DATA = [
     label: "LinkedIn URL",
     tooltip: "My LinkedIn profile",
     icon: FaLinkedin,
+  },
+  {
+    url: STACKOVERFLOW_URL,
+    label: "StackOverflow URL",
+    tooltip: "My StackOverflow profile",
+    icon: FaStackOverflow,
   },
   {
     url: MEDIUM_URL,
